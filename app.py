365d20dc8364fd7b8bbc0d6bd64b86da423d6fee
ifizzyou <pjh15237213@gmail.com>
2022-01-11 14:27:55 +0900

    branchTest

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'SPARTA'
 # client = MongoClient('3.38.99.10', 27017, username="test", password="test")
 client = MongoClient('localhost', 27017)
 db = client.db_hanghae99_miniproject1
-
+# branch 연습
 
 @app.route('/')
 def mainpage():
